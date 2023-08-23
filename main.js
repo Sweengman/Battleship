082bc53a564911d1c31e7eb848e0e15619366aab
Sweeney Corniea <sweenfi@icloud.com>
2023-08-23 07:15:29 -0600

    beef up html, ensure the beginning init of my program is responsive

--- main.js
+++ main.js
@@ -11,16 +11,44 @@ const COLOR_LOOKUP = {
 // ## variables
 let turn // 1 || -1
 let turnAfil //object holding affiliations 
+let currentAfil //currentAfil
 let winner // null || 1 || -1
 let shooting // true || false (will be used to help event listener either place ships or shoot where div is clicked)
 let pBoard // array[0Array....9Array] nArray[0...9]
 let p2Board// array[0Array....9Array] nArray[0...9]
+let pAffill
+let p2Affil
 let player
 let player2
 let shipSelect = {
     assignedShip: null,
     shipO: 1
 }
+let shipsPlaced = 0
+
+// Cached Dom Elements
+const opponentBoardEl = document.querySelector('#opponent-map')
+const playerBoardEl = document.querySelector('#player-map')
+const opponentSelectorEls = [...document.querySelectorAll('#opponent-map > div')]
+const playerSelectorEls = [...document.querySelectorAll('#player-map > div')]
+const messageEl =document.querySelector('h2')
+const playerInfoEl = document.querySelector('#pl-info')
+const player2InfoEl = document.querySelector('#comp-info')
+const cannonshotEl = [...document.querySelectorAll('#cannon-type > div')]
+const bodyEl = document.querySelector('body')
+// affiliation els
+const pirateButtonEl = document.querySelector('#pirate-button')
+const navyButtonEl = document.querySelector('#navy-button')
+// ship placement els
+// const pirateShips = [...document.querySelector('#pship-select')]
+// const navyShips = [...document.querySelector('#nship-select')]
+const pshipEls = document.querySelector('#pship-select')
+const nshipEls = document.querySelector('#nship-select')
+const retryButtonEl = document.querySelector('#retry')
+
+console.log(pirateButtonEl)
+console.log(navyButtonEl)
+
 // ## classes
 
 class Ship {
@@ -59,20 +87,21 @@ class Player {
             spreadShot: 1
         }
 
-        this.ships = [generateShips()]
+        this.ships = []
+        
 
-        this.shipsDestroyed = countShipDeath()
+        this.shipsDestroyed = false
     }
 
     generateShips(affiliation) {
-        for(let i = 1; i < 6; i++) {
-            return new Ship(affiliation, i) 
+        for(let i = 1; i < 5; i++) {
+            this.ships[i-1] = new Ship(affiliation, i) 
         }
     }
 
     countShipDeath() {
         deadArray = this.ships.filter(filterShips(ship))
-        return deadArray.length
+        this.shipsDestroyed = deadArray.length = 4 ? true : false
     }
 
     filterShips(ship) {
@@ -80,61 +109,33 @@ class Player {
     }
 }
 
-// Cached Dom Elements
-const opponentBoardEl = document.querySelector('#opponent-map')
-const playerBoardEl = document.querySelector('#player-map')
-const opponentSelectorEls = [...document.querySelectorAll('#opponent-map > div')]
-const playerSelectorEls = [...document.querySelectorAll('#player-map > div')]
-const messageEl =document.querySelector('h2')
-const playerInfoEl = document.querySelector('#pl-info')
-const player2InfoEl = document.querySelector('#comp-info')
-const cannonshotEl = [...document.querySelectorAll('#cannon-type > div')]
-const bodyEl = document.querySelector('body')
-// affiliation els
-const pirateButtonEl = document.querySelector('#pirate-button')
-const navyButtonEl = document.querySelector('#navy-button')
-// ship placement els
-// const pirateShips = [...document.querySelector('#pship-select')]
-// const navyShips = [...document.querySelector('#nship-select')]
-const pshipEls = [...document.querySelectorAll('#pship-select>div')]
-const nshipEls = [...document.querySelectorAll('#nship-select>div')]
-const retryButtonEl = document.querySelector('#retry')
 
-
+console.log(opponentSelectorEls)
 // Event Listeners
 
 //               choose affiliation
-if (pirateButtonEl){
-    pirateButtonEl.addEventListener('click', initialize)
-}
-
-if (navyButtonEl) {
-    navyButtonEl.addEventListener('click', initialize)
-}
+pirateButtonEl.addEventListener('click', initialize)
+navyButtonEl.addEventListener('click', initialize)
 
 //             choose grid div for placing ships and shooting squares
-if (opponentSelectorEls) {
-    opponentSelectorEls.addEventListener('click', shoot)
-}
-if (playerSelectorEls) {
-    playerSelectorEls.addEventListener('click', shoot)
-}
-
+opponentBoardEl.addEventListener('click', shoot)
+playerBoardEl.addEventListener('click', shoot)
 pshipEls.addEventListener('click', selectShip)
 nshipEls.addEventListener('click', selectShip)
 
 function selectShip(e) {
     if (shooting === true) return
-    else if(e.target.parentNode.id = turnAfil[turn.toString()]) {
+    else if(e.target.parentNode.id = currentAfil) {
         if (shipSelect.assignedShip === e.target.id) {    
             shipSelect.shipO *= -1
+            e.target.style.transform.rotate('90deg');
             }
         } else {shipSelect = {
             assignedShip: e.target.id,
             shipO: 1
-            
         }
         }
+    e.target.removeAttribute('id')
 }
 
 
@@ -142,71 +143,89 @@ function selectShip(e) {
 
 // Functions
 function initialize(e) {
-    player = new Player(1, e.target.id)
-    player2 = e.target.id = 'navy' ? new Player(-1, 'pirate') : new Player(-1, 'navy')
-    console.log(player2)
+    console.log('initialize!')
+    console.log(e.target.class)
+    pAffill = e.target.className
+    if (pAffill === 'pirate') {
+        p2Affil = 'navy'
+    } else if (pAffill === 'navy') {
+        p2Affil = 'pirate'
+    }
+    player = new Player(1, pAffill)
+    player2 = new Player(-1, p2Affil)
+    player.generateShips
+    player2.generateShips
     pBoard = []
     p2Board = []
     shooting = false
     turn = 1
     turnAfil = {
-        '1': e.target.id, 
+        '1': pAffill, 
         '-1': p2Affil
     }
-    createBoard(pBoard, e.target.id)
-    createBoard(p2Board, p2Affil) 
+    currentAfil = turnAfil[turn.toString()]
+    console.log(currentAfil)
+    createBoard(pBoard, playerBoardEl, pAffill)
+    createBoard(p2Board, opponentBoardEl, p2Affil) 
     render()
 
 }
 // creates a 10/10 grid in a board array to map player choices
 // also creates a div grid in html for 100 divs, each one with a unique ID (affiliation)(row)(column)
-function createBoard(boardArray, affil) {  
+function createBoard(boardArray, whosBoard, affil) {  
     for(let i = 0; i < 10; i++) {
         let num1 = i
+        boardArray.push([])
         for(let i = 0; i < 10; i++) {
-            boardArray.push(0)
+            boardArray[num1].push(0) 
             let num2 = i
             el = document.createElement('div')
-            let coordinate = el.setAtttribute('id', `${affil}-${num1}-${num2}`)
-            if (turn === 1) {
-                playerBoardEl.appendChild(coordinate)
-                playerBoardEl.setAttribute('class', affil)
-            } else if (turn === -1) {
-                opponentBoardEl.appendChild(coordinate)
-                opponentBoardEl.setAttribute('class', affil)
-            }
+            whosBoard.appendChild(el).setAttribute('id', `${affil}-${num1}-${num2}`)
+            //whosBoard.classList.add(affil)
+
         }
     }
 }
 
 function render() {
-    renderBoard()
-    renderMessage()
+    if (shipsPlaced >= 10) {
+        shooting = true
+    }
+    //renderMessage()
     renderShips()
-    renderStats()
-    renderShots()
+    //renderStats()
+    //renderShots()
+    renderBoard(pBoard, pAffill)
+    renderBoard(p2Board, p2Affil)
 }
 
 function renderShips() {
     //makes sure it is needed (eval with shooting var)
     //makes player specific ships visible
     //renders ship visible on player's map after event listener
-    if (shooting === false){
-        let shipsPlaced = 0
-        while(shipsPlaced < 6) {
-            let shipSection = document.querySelector(`#${turnAfil[turn.toString]}`)
-            shipSection.style.visibility = 'visible'
-            renderShipLocation()
-        }       
-    } else if (shooting === true) {/*shoot div*/}
+    if(!shooting){
+        let shipSection = document.querySelector(`.${currentAfil}`)
+        shipSection.style.visibility = 'visible'
+    } else if (shooting) {/*check for shot divs*/}
 }
-
-function renderShipLocation () {
-    if (shipSelect.assignedShip === true) {
-
+function renderBoard (board, affil) {
+    for (let i = 0; i > 10; i++) {
+        for (let innerArr of board[i]){
+            if (innerArr !== 0) {
+            let idx2 = array.findIndex(innerArr)
+            let element = document.getElementById(`${affil}-${idx1}-${idx2}`)
+            element.setAttribute('id', `${board[idx1][idx2]}`)
+            }
+        }
     }
 }
-
+    //     colArr.forEach(function()) {
+    //     const divId = `c${colIdx}r${board[2]}`;
+    //         // const cellEl = document.getElementById(divId);
+    //         // cellEl.style.backgroundColor = COLOR_LOOKUP[board[1]];
+    //     $(`#${divId}`).css('backgroundColor', COLOR_LOOKUP[board[1]])
+    //     }})
+    // }
 
         /*function grabShip(e) {
     player.ships[e.target.id]           #0-4
@@ -230,7 +249,7 @@ function shoot(e) { //e = event
     if (shooting) {
         render()
     } else if (!shooting) {
-        if (e.target.parentNode.id === turnAfil[turn.toString]) {//dont forget! board grid div ids will give coordinates for javascript board arrays
+        if (e.target.parentNode.id === currentAfil) {//dont forget! board grid div ids will give coordinates for javascript board arrays
             if (shipSelect.assignedShip === true) {//ship-selector div ids will indicate the exact ship (in theory. at very least it has the length of the ship )
                 let coordinateArray = e.target.id.split('-')
                 let coordinate1 = Number(coordinateArray[1])
@@ -241,6 +260,12 @@ function shoot(e) { //e = event
                 if (shipSelect.shipO === 1) {
                     if (shipLength + coordinate2 < 10 ) {
                         board[coordinate1].splice(coordinate2, shipLength, shipSelect.assignedShip)
+                        for(let i = 0; i< shipLength; i++) {
+                           let currentEl = document.getElementById
+                           (`${turnAfil[true.toString()]}-${coordinate1}-${coordinate2 + i}`)
+                           currentEl.style.backgroundColor = 'yellow'
+                        }
+
                     }
                 } else if(shipSelect.shipO === -1) {
                     if (shipLength + Number(corrdinateArray[1]) < 10 ) {
@@ -249,10 +274,13 @@ function shoot(e) { //e = event
                         }
                     }
                     }
+                    shipsPlaced ++
                 }
             }
         }
-    render()} 
+    render()
+    turn *= -1
+    } 
 }
 
 function checkForShip(el) {
